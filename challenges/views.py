b8d6132534e28f927f71632010ea3ef35bbb2d62
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-15 05:35:28 +0300

    Redirects & Adding More Dynamic View Logic

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+
+monthly_challenges = {
+  'january': "This is january",
+  'february': "This is february!",
+  'march': "This is march!"
+}
+
+
+def monthly_challenge_by_number(request, month):
+  months = list(monthly_challenges.keys())
+  if month > len(months):
+    return HttpResponseNotFound("Invalid month")
+  redirect_month = months[month - 1]
+  return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
-  challenge_text = None
+  try:
+    challenge_text = monthly_challenges[month]
+    return HttpResponse(challenge_text)
+  except:
+    return HttpResponseNotFound("This not page 404")
 
-  if month == 'january':
-    challenge_text = "This is january"
-  elif month == 'february':
-    challenge_text = "This is february!"
-  elif month == 'march':
-    challenge_text = "This is march!"
-  else:
-    return HttpResponseNotFound("This month is not supported!")
-  return HttpResponse(challenge_text)
